455c9c56b78003fc7b43ee9558f9d8083644717a
OFIR ALPERSHTEIN <ofiralpershtein@OFIRs-MacBook-Pro.local>
2020-10-22 17:25:38 -0600

    Refactored to a user input only

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,9 +10,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       value: '',
-      moviesData: exampleMoviesData
-      // userMovies: ''
-      // errorMessage: errorMessage
+      moviesData: []
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -20,22 +18,11 @@ class App extends React.Component {
   }
 
   handleChange(event) {
-    // this.setState({moviesSubmit: exampleMoviesData})
-    if (event.target.value === '') {
-      this.setState({moviesData: exampleMoviesData});
-    }
     this.setState({value: event.target.value.toLowerCase()}, function(){console.log('cb state:', this.state)});
 
   }
-  // handleError(event) {
-  //   if (!handleSubmit(event)) {
-  //     this.setState(errorMessage: )
-  //   }
-  // }
   handleSubmit(event) {
     event.preventDefault();
-    console.log('event from handle submit:', event)
-    // this.setState({moviesSubmit: exampleMoviesData})
     var result = [];
     for (var i = 0; i < this.state.moviesData.length; i++) {
       var movieTitle = this.state.moviesData[i].title.toLowerCase();
@@ -43,22 +30,16 @@ class App extends React.Component {
         result.push(this.state.moviesData[i]);
       }
     }
-    // if (result.length === 0) {
-    //   this.setState(moviesData: {});
-    // }
       this.setState({moviesData: result}, function() {
         console.log('current movie data:',this.state.moviesData);
       });
-    // console.log('result:', result)
   }
 
   addMovie(event) {
-    // when a user clicks submit I want to take the input, create an object for it, and add it to an array, change the state to display user movies only.
     event.preventDefault();
-    var value = event.target
-    console.log('this.state.value from addMovie:', this.state.value)
+    var array = [...this.state.moviesData, {title: this.state.value}]
     this.setState({
-      moviesData: [{title: this.state.value}]
+      moviesData: array
     })
   }
 
@@ -70,14 +51,11 @@ class App extends React.Component {
           <AddMovie onAddMovie={this.addMovie} onAddMovieChange={this.handleChange}/>
         </div>
         <div className="search">
-          <Searchbar search={exampleMoviesData} onInputChange={this.handleChange} onSearch={this.handleSubmit} />
+          <Searchbar  onInputChange={this.handleChange} onSearch={this.handleSubmit} />
         </div>
         <div>
           <MovieList movies={this.state.moviesData} />
         </div>
-        {/* <div class="error">
-
-        </div> */}
       </div>
     );
   }
--- src/components/UserMovies.jsx
+++ src/components/UserMovies.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 
 var AddMovie = (props) => {
-  console.log('props from user movies:',props)
   return (
     <form onSubmit={props.onAddMovie}>
-      <input type="text" className="inputBox" name="movie-input" onChange={props.onAddMovieChange}/>
+      <input type="text" className="inputBox" name="movie-input" placeholder="Add a movie" onChange={props.onAddMovieChange}/>
       <button className="button">Add</button>
     </form>
   )
